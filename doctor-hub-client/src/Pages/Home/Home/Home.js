58f96338da5d70bba56a3705e7708453708e51f4
ahm-fahim <contactahmfahim@gmail.com>
2023-05-22 16:49:54 +0600

    address seciton done

--- doctor-hub-client/src/Pages/Home/Home/Home.js
+++ doctor-hub-client/src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../Hero/Hero";
+import Address from "../Address/Address";
 
 const Home = () => {
     return (
         <div>
             <Hero />
+            <Address />
         </div>
     );
 };
