44704203b437dc72082ff06219cf0a1c5312d5e2
ahm-fahim <contactahmfahim@gmail.com>
2023-05-24 22:34:53 +0600

    modal update

--- doctor-hub-client/src/Pages/Home/ServiceSection/DoctorsServices/DoctorsDetails.js
+++ doctor-hub-client/src/Pages/Home/ServiceSection/DoctorsServices/DoctorsDetails.js
@@ -38,27 +38,53 @@ const DoctorsDetails = ({ info }) => {
                 Get Appoint
             </Link>
 
-            <Modal className="lg:p-10" isOpen={isOpen} onClose={closeModal}>
-                <img src={image} alt="" className="h-40" />
-                <h1 className="text-2xl text-gradient font-bold mt-5">{name}</h1>
-                <h2 className="text-lg mt-3">
-                    {" "}
-                    <span className="font-bold">Specialty - </span> {specialty}
-                </h2>
-                <h3 className="text-xs text-gradient mt-">{experience}</h3>
-                <h3 className="text-lg mt-2">
-                    <span className="font-bold">Education - </span>
-                    {education}
-                </h3>
-                <h4 className="text-md mt-2">
-                    <span className="font-bold">Hospital - </span>
-                    {hospital}
-                </h4>
-                <p className="text-sm text-gray-500 mt-2">
-                    {" "}
-                    <span className="font-bold text-black">Description - </span>{" "}
-                    {description}
-                </p>
+            <Modal className="lg:p-16" isOpen={isOpen} onClose={closeModal}>
+                <div className="flex justify-between flex-row-reverse items-start">
+                    <button
+                        className="text-center text-gray-600 hover:text-gray-800"
+                        onClick={closeModal}
+                    >
+                        <svg
+                            className="w-12 h-12 fill-current"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                fillRule="evenodd"
+                                d="M10 11.414l4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95L.636 16.364l4.95-4.95L.636 6.464 1.414 5.05l4.95 4.95L11.364 5.05l.778 1.414-4.95 4.95z"
+                            />
+                        </svg>
+                    </button>
+                    <div>
+                        <img src={image} alt="" className="h-40" />
+                        <h1 className="text-2xl text-gradient font-bold mt-5">
+                            {name}
+                        </h1>
+                        <h2 className="text-lg mt-3">
+                            {" "}
+                            <span className="font-bold">Specialty - </span>{" "}
+                            {specialty}
+                        </h2>
+                        <h3 className="text-xs text-gradient mt-">
+                            {experience}
+                        </h3>
+                        <h3 className="text-lg mt-2">
+                            <span className="font-bold">Education - </span>
+                            {education}
+                        </h3>
+                        <h4 className="text-md mt-2">
+                            <span className="font-bold">Hospital - </span>
+                            {hospital}
+                        </h4>
+                        <p className="text-sm text-gray-500 mt-2">
+                            {" "}
+                            <span className="font-bold text-black">
+                                Description -{" "}
+                            </span>{" "}
+                            {description}
+                        </p>
+                    </div>
+                </div>
             </Modal>
         </div>
     );
--- doctor-hub-client/src/Pages/Home/ServiceSection/DoctorsServices/Modal.js
+++ doctor-hub-client/src/Pages/Home/ServiceSection/DoctorsServices/Modal.js
@@ -10,30 +10,12 @@ const Modal = ({ isOpen, onClose, children }) => {
         <div className={modalClasses}>
             {/* Background overlay */}
             <div
-                className="fixed inset-0 bg-gray-900 opacity-50"
+                className="fixed inset-0 bg-gradient2 opacity-60"
                 onClick={onClose}
             ></div>
 
             {/* Modal content */}
             <div className="bg-white p-6 rounded-lg shadow-lg z-10">
-                {/* Close button */}
-                <button
-                    className="absolute top-4 right-4 text-gray-500 hover:text-gray-700"
-                    onClick={onClose}
-                >
-                    <svg
-                        className="w-6 h-6 fill-current"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            fillRule="evenodd"
-                            d="M10 11.414l4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95L.636 16.364l4.95-4.95L.636 6.464 1.414 5.05l4.95 4.95L11.364 5.05l.778 1.414-4.95 4.95z"
-                        />
-                    </svg>
-                </button>
-
-                {/* Modal content */}
                 {children}
             </div>
         </div>
