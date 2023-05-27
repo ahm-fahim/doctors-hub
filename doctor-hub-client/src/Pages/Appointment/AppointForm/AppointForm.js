0e4ab945ba3ced0aeb382957bcc9061bd27d286e
ahm-fahim <contactahmfahim@gmail.com>
2023-05-27 20:24:10 +0600

    doctors time slots added

--- doctor-hub-client/src/Pages/Appointment/AppointForm/AppointForm.js
+++ doctor-hub-client/src/Pages/Appointment/AppointForm/AppointForm.js
@@ -3,39 +3,40 @@ import PrimaryButton from "../../../components/PrimaryButton/PrimaryButton";
 
 const AppointForm = ({ doctorInfo, currentDate, appointInfo }) => {
     const { name, specialty } = doctorInfo;
-
-    console.log("appoint Info", doctorInfo);
+    console.log("appoint Info", appointInfo);
 
     return (
         <div className="hero">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <div className="card flex-shrink-0 w-full max-w-sm">
                     <form className="card-body">
-                        <div>
-                            <input
-                                type="text"
-                                value={`${name}`}
-                                readOnly
-                                className="outline-none  text-gradient text-xl font-bold"
-                            />
-                            <input
-                                type="text"
-                                value={`${specialty}`}
-                                readOnly
-                                className="outline-none text-xs font-bold text-gray-500"
-                            />
-                        </div>
+                        <input
+                            type="text"
+                            value={`${name}`}
+                            readOnly
+                            className="outline-none  text-gradient text-xl font-bold"
+                        />
+                        <input
+                            type="text"
+                            value={`${specialty}`}
+                            readOnly
+                            className="outline-none text-xs font-bold text-gray-500"
+                        />
                         <input
                             type="text"
                             value={currentDate.props.children}
                             readOnly
                             className="outline-none text-gradient font-bold form-control"
                         />
-
+                        <select className="select select-accent w-full max-w-xs">
+                            {appointInfo.map((schedule ,index) => (
+                                <option className="text-gradient" key={index} value={schedule}>{schedule}</option>
+                            ))}
+                        </select>
                         <input
                             type="text"
                             placeholder="Full Name"
-                            className="input mt-3 input-bordered form-control"
+                            className="input input-bordered form-control"
                         />
                         <input
                             type="text"
